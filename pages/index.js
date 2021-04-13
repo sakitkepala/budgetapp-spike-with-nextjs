f24ff9c9929872503a3767a02a0e43e56d5353e4
sakitkepala <andikapriyotama@gmail.com>
2021-04-14 05:20:38 +0700

    Init commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+      <div className="depan">
+        <main className="lembar">
+          <h1 className="judul">Pake shortcut dong jaman now</h1>
+        </main>
+      </div>
+  );
+}
