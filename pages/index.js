41c2630e7e4d5f44403550fed4ead62e024f2106
sakitkepala <andikapriyotama@gmail.com>
2021-04-15 00:06:18 +0700

    Requirement interaksi hoho

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
+/**
+ * Fitur-fitur kunci yang dibutuhkan:
+ * 1. Memanggil UI dengan shortcut (hotkey), terutama Palet Perintah. Kebanyakan tindakan
+ *    di aplikasi ini pakai keyboard & shortcut & perintah dari Palet, contoh:
+ *    > Menambahkan dana ke anggaran: dimulai dengan panggil Palet, ketikkan perintah,
+ *      UI sekuensial untuk isi nominal anggaran, dsb.  UI-nya ada di Palet, atau gak jauh-
+ *      jauh dari Palet.
+ *    >
+ * 2. Mengelola fokus komponen atau elemen yang aktif dengan keyboard juga: tab, tombol
+ *    panah, access key, dsb.
+ *    > Pindah-pindah cell di tabel anggaran, misalnya, juga bisa dilakukan pakai keyboard,
+ *      tanda panah terutama, dan di cell yang fokus, nilainya bisa langsung diedit.
+ */
+
 import {
   PenyiarPerintahProvider,
   usePenyiarPerintah,
