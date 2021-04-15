97ca598472d0bd5bde5d86e353491e982bc49b6e
sakitkepala <andikapriyotama@gmail.com>
2021-04-16 01:43:36 +0700

    REF tes Palet

--- components/__tests__/test.palet-perintah.js
+++ components/__tests__/test.palet-perintah.js
@@ -1,8 +1,24 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import { PaletPerintah, InputPerintah } from "../palet-perintah";
 import { PenyiarPerintahProvider } from "../penyiar-perintah";
+import { PaletPerintah, InputPerintah } from "../palet-perintah";
+// import { PaletPerintah, InputPerintah } from "../mock.palet-perintah";
 
-function userShortcut(kombinasi) {
+function simulasiKeyUser(target, keyCode, shiftKey = false) {
+  fireEvent.keyDown(target, { keyCode, shiftKey });
+  fireEvent.keyUp(target, { keyCode, shiftKey });
+}
+
+/**
+ * Menyimulasikan user mengeksekusi shortcut kibor dari elemen yang sedang fokus.
+ *
+ * Default-nya, shortcut "didengarkan" oleh elemen yang sedang fokus (aktif) akibat
+ * adanya aksi-aksi sebelumnya: user klik di suatu tempat, ngisi form, etc. Maka
+ * dari itu, fokus didefaultkan ke `document.activeElement`, kecuali user menentukan
+ * mau aktif dimana.
+ * @param {string} kombinasi shortcut kibor yang dieksekusi
+ * @param {HTMLElement} target (opsional) elemen target event-nya dimunculkan, defaultnya `document.activeElement`
+ */
+function userShortcut(kombinasi, target = document.activeElement) {
   switch (kombinasi) {
     case "shift+p":
       /**
@@ -20,24 +36,19 @@ function userShortcut(kombinasi) {
        * dan key SHIFT di bacanya bukan dengan mendeteksi event dari SHIFT itu sendiri
        * melainkan dibaca dari apakah nilai modifier `shiftKey`-nya benar atau enggak.
        */
-      fireEvent.keyDown(document.body, {
-        keyCode: 80 /* tombol kibor "P" */,
-        shiftKey: true,
-      });
+      simulasiKeyUser(target, 80, true); /* tombol kibor "P" === 80 */
       break;
 
     case "esc":
-      fireEvent.keyDown(document.body, {
-        keyCode: 27 /* tombol kibor "ESC" */,
-      });
+      simulasiKeyUser(target, 27); /* tombol kibor "ESC" === 27 */
       break;
 
     default:
-      break;
+      throw new Error("Shortcut gak didukung!");
   }
 }
 
-test("Palet Perintah cuma muncul saat eksekusi shortcut SHIFT+P", () => {
+test("Palet Perintah awal render cuma muncul setelah eksekusi shortcut SHIFT+P", () => {
   render(
     <PenyiarPerintahProvider>
       <PaletPerintah>
@@ -65,4 +76,21 @@ test("Palet Perintah ditutup pakai tombol ESC", () => {
   userShortcut("esc");
 
   expect(screen.queryByLabelText(/palet perintah/i)).not.toBeInTheDocument();
+});
+
+test("Palet Perintah bisa dibuka lagi pakai shortcut setelah ditutup", () => {
+  render(
+    <PenyiarPerintahProvider>
+      <PaletPerintah>
+        <InputPerintah />
+      </PaletPerintah>
+    </PenyiarPerintahProvider>
+  );
+
+  // WORKS!
+  userShortcut("shift+p");
+  userShortcut("esc");
+  userShortcut("shift+p", window.document.body.querySelector("div"));
+
+  expect(screen.queryByLabelText(/palet perintah/i)).toBeInTheDocument();
 });
